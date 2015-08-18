45a35c1227978e3b8b45ac486484f0ed996c32ac
Hiroshi Urabe <mail@torounit.com>
2015-08-19 02:56:30 +0900

    repo init

---
+++ admin-posts-columns-manager.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Admin-posts-columns-manager
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: admin-posts-columns-manager
+Domain Path: /languages
+*/
+
